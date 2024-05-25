ff1f87aa51cac007ac5485b728ea38cfc360a2bb
SJ <davidqwertyuiop39@gmail.com>
2024-05-25 17:57:56 +0100

    modified:   lib/sensor.dart

--- lib/sensor.dart
+++ lib/sensor.dart
@@ -5,19 +5,26 @@ class Sensor {
   const Sensor({
     required this.humidity,
     required this.temperature,
+    required this.soilMoisture,
   });
 
   Sensor.fromJson(Map<String, Object?> json)
       : this(
             humidity: json['Humidity'] == null
                 ? 0.0
-                : (double.parse(json['Humidity']! as String)),
+                : double.parse(json['Humidity']! as String),
             temperature: json['Temperature'] == null
                 ? 0.0
-                : double.parse(json['Temperature'] as String));
+                : double.parse(json['Temperature']! as String),
+            soilMoisture: json['SoilMoisture'] == null
+                ? 0.0
+                : double.parse(json['SoilMoisture']! as String),
+        );
 
   final double humidity;
   final double temperature;
+  final double soilMoisture;
+
 
   Map<String, Object?> toJson() {
     return {
