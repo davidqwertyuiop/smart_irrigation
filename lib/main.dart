021aca3315f42424ea850637a0cdca48af9946d8
SJ <davidqwertyuiop39@gmail.com>
2024-05-28 14:01:24 +0100

    modified:   analysis_options.yaml 	modified:   lib/main.dart 	modified:   lib/screens/home_screen.dart 	modified:   pubspec.yaml 	l10n.yaml 	lib/classes/ 	lib/l10n/

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application_2/screens/screen.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+import 'classes/language_constants.dart';
 import 'constants.dart';
 import 'firebase_options.dart';
 
@@ -18,8 +19,29 @@ WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key,  });
+@override
+  State<MyApp> createState() => _MyAppState();
+    static void setLocale(BuildContext context, Locale newLocale) {
+    _MyAppState? state = context.findAncestorStateOfType<_MyAppState>();
+    state!.setLocale(newLocale);
+  }
+  }
+  class _MyAppState extends State<MyApp> {
+  Locale? _locale;
+
+  setLocale(Locale locale) {
+    setState(() {
+      _locale = locale;
+    });
+  }
+
+  @override
+  void didChangeDependencies() {
+    getLocale().then((locale) => {setLocale(locale)});
+    super.didChangeDependencies();
+  }
 
   // This widget is the root of your application.
   @override
@@ -33,6 +55,7 @@ class MyApp extends StatelessWidget {
       colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       useMaterial3: true,
       ),
+      locale: _locale,
       home: const WelcomePage(),
     );
   }
